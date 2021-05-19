28de7e18400581cbbe8aaec2489ee2b187bde1c8
TinaYang <60873217+Tina04021997@users.noreply.github.com>
2021-05-19 16:40:15 +0800

    Update DESeq2.R

--- DESeq2.R
+++ DESeq2.R
@@ -12,3 +12,14 @@ GE_counts <- GE[,c(1,7:12)]
 colnames(GE_counts) <- c("Gene","ZT02(R1)","ZT02(R2)","ZT02(R3)","ZT06(R1)","ZT06(R2)","ZT06(R3)")
 # Save as csv file
 write.csv(GE_counts, file = "GE_counts.csv", row.names = F)
+# Create the coldata.csv (sample information) with excel 
+# Import coldata.csv and cts files
+coldata <- read.csv("coldata.csv", row.names = 1)
+cts <- as.matrix(read.csv("GE_counts.csv", row.names = 1))
+# Correct the colnames of cts 
+colnames(cts) <- row.names(coldata)
+# Convert coldata from characters to factors
+coldata$time.length <- factor(coldata$time.length)
+coldata$type <- factor(coldata$type)
+# Make sure that the columnm names of cts == row names of coldata
+all((rownames(coldata)) %in% colnames(cts))
